97110657d57bb6484112fda8eff98058ca711f26
Dariusz <s17084@pjwstk.edu.pl>
2020-03-29 14:05:14 +0200

    Dodanie projektu do repo

---
+++ Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kulig, Rychlik, Wawrzeńczyk";
+        }
+    }
+}
